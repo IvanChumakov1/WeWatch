fbde5e9a95228baf09cd5aa05b91bdd4b0739be7
IvanChumakov1 <ivanchumakov228@gmail.com>
2024-04-10 06:21:35 +0700

    Add UI

--- app/src/main/java/com/example/watch/AddActivity.kt
+++ app/src/main/java/com/example/watch/AddActivity.kt
@@ -1,20 +1,31 @@
 package com.example.watch
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.watch.databinding.ActivityAddBinding
+import com.example.watch.databinding.ActivityMainBinding
 
 class AddActivity : AppCompatActivity() {
+    lateinit var bindingClass : ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindingClass = ActivityAddBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add)
+        setContentView(bindingClass.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun goToSearchActivity(view: View){
+        val intent = Intent(this, SearchActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/watch/MainActivity.kt
+++ app/src/main/java/com/example/watch/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.watch
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.watch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingClass : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(bindingClass.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setSupportActionBar(bindingClass.toolbar2)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.deleteMenuItem ){
+            // Событие
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // Функция проверки размера списка
+
+    //
+    fun goToAddActivity(view: View){
+        val intent = Intent(this, AddActivity::class.java)
+        startActivity(intent)
     }
 }
